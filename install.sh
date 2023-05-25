40e54dd0f49731f452b1c6e85c96c522ac5febef
Moss <moss@mossx.net>
2023-05-24 23:03:35 -0700

    Meta: Making This A Single Repo Instead of Multiple
    Moved firmware files into firmware folder.

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 "sudo" echo -n
 make -C build
 "sudo" mount /dev/sda1 /home/moss/usb/
-"sudo" cp ./build/mage_firm.uf2 /home/moss/usb/
+"sudo" cp ./build/firmware/mage_firm.uf2 /home/moss/usb/
 "sudo" umount /home/moss/usb/
