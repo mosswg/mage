8ce8bb6c1c19fd0eae951f0506fbf24178235229
Moss <moss@mossx.net>
2023-05-25 23:18:56 -0700

    Firmware: Changed to Not Mount, Upload, or Unmount if Build Fails

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
 "sudo" echo -n
-make -C build
-"sudo" mount /dev/sda1 /home/moss/usb/
-"sudo" cp ./build/firmware/mage_firm.uf2 /home/moss/usb/
-"sudo" umount /home/moss/usb/
+make -C build && "sudo" mount /dev/sda1 /home/moss/usb/ && "sudo" cp ./build/firmware/mage_firm.uf2 /home/moss/usb/ && "sudo" umount /home/moss/usb/
